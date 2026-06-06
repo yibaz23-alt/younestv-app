26baeaa879ec23ba4a47359456c3deedc2b6ad42
yibaz23-alt <yibaz23@gmail.com>
2026-06-06 02:33:31 +0100

    Remove Android configuration from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,28 +2,3 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-
-android {
-    namespace = "com.younestv.app"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.younestv.app"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-}
