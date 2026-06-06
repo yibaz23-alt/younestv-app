f160403e6f53ba9cc2b393afe0a6cf24e9a0946e
yibaz23-alt <yibaz23@gmail.com>
2026-06-06 02:32:27 +0100

    Add Android and Kotlin plugin versions

--- Settings.gradle.kts
+++ Settings.gradle.kts
@@ -4,6 +4,10 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.3.0"
+        id("org.jetbrains.kotlin.android") version "1.9.22"
+    }
 }
 rootProject.name = "younestv-android"
 include(":app")
