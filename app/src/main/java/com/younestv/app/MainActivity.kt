98f5b504e459de11a1a0754d5a32267cb3fc0f88
yibaz23-alt <yibaz23@gmail.com>
2026-06-06 02:36:38 +0100

    Replace WebView with TextView for welcome message
    Removed WebView implementation and added a TextView to display a welcome message.

--- app/src/main/java/com/younestv/app/MainActivity.kt
+++ app/src/main/java/com/younestv/app/MainActivity.kt
@@ -1,35 +1,15 @@
 package com.younestv.app
-
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var webView: WebView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        webView = WebView(this)
-        setContentView(webView)
-
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-
-        webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlDispling(view: WebView, url: String): Boolean {
-                return false
-            }
-        }
-
-        webView.loadUrl("https://www.flashscore.ma/football/world/كاس-العالم/")
-    }
-
-    override fun onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
+        val text = TextView(this)
+        text.text = "مرحبا بيك فـ YounesTV 🎉"
+        text.textSize = 24f
+        text.setPadding(50, 200, 50, 50)
+        setContentView(text)
     }
 }
