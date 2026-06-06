748e75476ddcaee5d1e6339a44a686b1e6ac6d8b
yibaz23-alt <yibaz23@gmail.com>
2026-06-06 01:01:14 +0100

    Implement WebView in MainActivity for web content

---
+++ app/src/main/java/com/younestv/app/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.younestv.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlDispling(view: WebView, url: String): Boolean {
+                return false
+            }
+        }
+
+        webView.loadUrl("https://www.flashscore.ma/football/world/كاس-العالم/")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
